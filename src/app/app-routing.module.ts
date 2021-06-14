d16cdc177beec845a07838bfebaf6e6fa495b1ac
Сергей Захаров <sergei86zahar@mail.ru>
2021-06-14 22:28:34 +0700

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AboutComponent} from "./pages/about-page/about/about.component";
+import {InitComponent} from "./pages/home-page/components/init/init.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'about', component: AboutComponent},
+  {path: '', component: InitComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InitComponent } from './pages/home-page/components/init/init.component';
+import { AboutComponent } from './pages/about-page/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
