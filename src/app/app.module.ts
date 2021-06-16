88a7871bb6e24db94076b5c0c17f090ada5df9ab
Сергей Захаров <sergei86zahar@mail.ru>
2021-06-16 13:38:34 +0700

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InitComponent } from './pages/home-page/components/init/init.component';
 import { AboutComponent } from './pages/about-page/about/about.component';
-import { JjComponent } from './pages/home-page/components/jj/jj.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InitComponent,
     AboutComponent,
-    JjComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
